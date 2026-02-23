f86b9065e0360b55958b34490f2f61041b525eed
Bianca Montalvo <biancamontalvo@biancas-air-2.mynetworksettings.com>
2026-02-23 12:50:35 -0800

    burger menu

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Hello World')
+
+function openMenu() {
+    document.body.classList.toggle("menu--open");
+}
+
+function closeMenu() {
+    document.body.classList.remove("menu--open");
+}
